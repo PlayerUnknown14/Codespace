e0fbfa3f2ad278790117fde137377d40e3cb9f2c
PlayerUnknown14 <gordovvladislav@yandex.ru>
2024-11-17 18:37:54 +0700

    Какая-то лаба по ЯП

---
+++ C_codespace/programming languages/1 semester/laboratory work/12.11.24 - Fifth work/Vlad/laba.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b) {
+    return (*(int*)b - *(int*)a);
+}
+
+int main() {
+    int N, i;
+    int k1 = 0;
+    int k2 = 0;
+    int k3 = 0;
+
+    printf("Укажите количество элементов вашего массива: ");
+    scanf("%d", &N);
+    int X[N];
+
+    printf("Введите %d чисел (через пробел): ", N);
+    for(i = 0; i < N; i++)
+        scanf("%d", &X[i]);
+    int n = sizeof(X) / sizeof(int);
+    qsort(X, n, sizeof(int), compare);
+    printf("Упорядоченный по убыванию массив: ");
+    for (i = 0; i < N; i++)
+        {
+            printf("%d ", X[i]);
+            if (X[i] > 0)
+                k1++;
+            else if (X[i] < 0)
+                k2++;
+            else
+                k3++;
+        };
+    printf("\nКоличество положительных элементов - %d\nКоличество отрицательных элементов - %d\nКоличество нулевых элементов - %d", k1, k2, k3);
+    
+    return 0;
+}
