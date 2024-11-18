cfd08312947d1d707f931ee335ba4f54d1afbdd2
luvidmi <welles7777@gmail.com>
2024-11-18 23:27:20 +0700

    что-то пытался сделать в 5-ой лабе ОПИ

--- Файлы/1 курс/Основы программной инженерии/1 семестр/Лабораторные работы/Лабораторная работа 5/MathOperations.py
+++ Файлы/1 курс/Основы программной инженерии/1 семестр/Лабораторные работы/Лабораторная работа 5/MathOperations.py
@@ -1,5 +1,6 @@
 import time
 from random import randint
+import random
 from tkinter import *
 from tkinter import ttk
 from random import *
@@ -9,19 +10,22 @@ root = tk.Tk()
 root.title('MathOperations')
 root.geometry('500x500')
 
-def click():
+mass = []
+
+def clicked():
     text = Combobox.get()   
     if text == 'Сложение':
-        вычисления.sum()
+        вычисления.summa()
 class вычисления:
-    def sum():
+    def summa():
+        global mass
         amount = amount_entry.get()
         range = range_entry.get().split(' ')
-        a = []
-        for i in range(amount):
-            a.append(randint(range[0], range[1]))
+        rangemin = int(range[0])
+        rangemax = int(range[1])
+        mass = [randint(rangemin, rangemax) for i in range(int(amount))]
         start_time = time.time()
-        summass = sum(a)
+        summass = sum(mass)
         end_time = time.time()
         execution_time = end_time - start_time
         time_label.configure(text=f'Время выполнения программы: {execution_time} секунд')
@@ -51,7 +55,7 @@ range_label.place(x=6, y =num*2+20)
 range_entry = ttk.Entry()
 range_entry.place(x=6, y=num*3+20)
 
-start_button = ttk.Button(text='Старт', command=click)
+start_button = ttk.Button(text='Старт', command=clicked)
 start_button.place(x=6, y=num*4+20)
 
 time_label = ttk.Label(text='')
