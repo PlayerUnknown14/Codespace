0249f8ca0c298a9f7678cdb2128c7e544835c0f2
PlayerUnknown14 <gordovvladislav@yandex.ru>
2024-12-06 19:43:01 +0700

    6 лаба ЯП

--- C_codespace/programming languages/1 semester/laboratory work/26.11.24 - Sixth work/Vlad/laba.c
+++ C_codespace/programming languages/1 semester/laboratory work/26.11.24 - Sixth work/Vlad/laba.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
+
 int main(){
-    char text[100], word[10], substr_pointer;
+    char text[100];
+    char word[100];
+    char* strpoiner = text;
     int count = 0;
+
     printf("\nВведите строку текста: ");
     scanf("%s", &text);
+    puts(text);
     printf("\nВведите слово: ");
     scanf("%s", &word);
-    substr_pointer = strstr(text, word);
-    while (substr_pointer != NULL)
+    puts(word);
+
+    while ((strpoiner = strstr(strpoiner, word)) != NULL)
     {
             count++;
-            strncpy(substr_pointer,"    ",4);
-            substr_pointer = strstr(text, word);
+            strpoiner++;
     }
 
     printf("Указанное слово встречается в тексте %d раз.", count);
-
+    getchar();
     return 0;
 }
 
+/*
 #include <string.h>
 #include <stdio.h> 
 #define SIZE 100                     
@@ -50,3 +55,4 @@ int main()
     getchar();
     return 0;
 }
+*/
