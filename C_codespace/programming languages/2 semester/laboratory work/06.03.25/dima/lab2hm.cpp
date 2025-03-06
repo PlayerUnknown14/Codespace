de4d9b9036bbe362ae4c4a9eaaadd0ef72b3ae01
luvidmi <luvidmi@gmail.com>
2025-03-06 10:44:57 +0300

    лаб2 яп

---
+++ C_codespace/programming languages/2 semester/laboratory work/06.03.25/dima/lab2hm.cpp
@@ -0,0 +1,30 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+int countNum(char*filename, int a, int b){
+    ifstream fin;
+    fin.open(filename, ios::in);
+    if (!fin){
+        cout<<"\n Нет исходного файла";
+        return -1;
+    }
+
+    int count = 0;
+    int number;
+    while (fin>>number){
+        if (number > a && number < b){
+            count++;
+        }
+    }
+    fin.close();
+    return count;
+
+}
+
+int main(){
+    int a = 10; 
+    int b = 30;
+    int result = countNum("C://Users//luvidmi//Desktop//git//Codespace//C_codespace//programming languages//2 semester//laboratory work//06.03.25//dima//numbers.txt",a,b);
+    cout<<"Количество чисел, больших "<<a<<", но меньших "<<b<<" = "<<result;
+    return 0;
+}
