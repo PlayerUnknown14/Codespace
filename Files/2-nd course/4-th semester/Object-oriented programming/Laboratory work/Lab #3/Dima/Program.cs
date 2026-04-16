f593eee8c78b2360bfbb9b2b71877c7ae78bc35b
luvidmi <welles7777@gmail.com>
2026-04-16 23:52:15 +0700

    Лабы по ООП(4-6)

---
+++ Files/2-nd course/4-th semester/Object-oriented programming/Laboratory work/Lab #3/Dima/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Lab3
+{
+    // Базовый класс
+    abstract class Engine
+    {
+        public string Model { get; set; }
+        public int Power { get; set; }
+
+        public Engine(string model, int power)
+        {
+            Model = model;
+            Power = power;
+        }
+
+        // Виртуальный метод для вывода (полиморфизм)
+        public virtual void Print()
+        {
+            Console.WriteLine($"Двигатель: {Model}, Мощность: {Power}");
+        }
+
+        // Сравнение объектов по значениям 
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType()) return false;
+            Engine other = (Engine)obj;
+            return Model == other.Model && Power == other.Power;
+        }
+
+        public override int GetHashCode() => (Model, Power).GetHashCode();
+    }
+
+    // ДВС
+    class Ice : Engine
+    {
+        public int Cyls { get; set; } // Кол-во цилиндров
+
+        public Ice(string model, int power, int cyls) : base(model, power)
+        {
+            Cyls = cyls;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"ДВС: {Model}, Мощность: {Power}, Цилиндров: {Cyls}");
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!base.Equals(obj)) return false;
+            return Cyls == ((Ice)obj).Cyls;
+        }
+    }
+
+    // Дизель (наследует ДВС)
+    class Diesel : Ice
+    {
+        public double Fuel { get; set; } // Расход топлива
+
+        public Diesel(string model, int power, int cyls, double fuel) : base(model, power, cyls)
+        {
+            Fuel = fuel;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"Дизель: {Model}, Мощность: {Power}, Расход: {Fuel}л");
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!base.Equals(obj)) return false;
+            return Fuel == ((Diesel)obj).Fuel;
+        }
+    }
+
+    // Реактивный двигатель
+    class Jet : Engine
+    {
+        public int Thrust { get; set; } // Тяга
+
+        public Jet(string model, int power, int thrust) : base(model, power)
+        {
+            Thrust = thrust;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"Реактивный: {Model}, Мощность: {Power}, Тяга: {Thrust}кН");
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!base.Equals(obj)) return false;
+            return Thrust == ((Jet)obj).Thrust;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Engine[] list = new Engine[3];
+
+            list[0] = new Ice("V8", 400, 8);
+            list[1] = new Diesel("D-100", 250, 4, 12.5);
+            list[2] = new Jet("Turbo-X", 10000, 80);
+
+            Console.WriteLine("Список двигателей:");
+            foreach (var e in list)
+            {
+                e.Print();
+            }
+
+            // Проверка Equals
+            Diesel d1 = new Diesel("Test", 100, 4, 10);
+            Diesel d2 = new Diesel("Test", 100, 4, 10);
+            Console.WriteLine("\nОбъекты d1 и d2 равны: " + d1.Equals(d2));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Files/2-nd course/4-th semester/Object-oriented programming/Laboratory work/Lab #4/Dima/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lab4
+{
+    // Класс Time теперь реализует IComparable для сортировки "по умолчанию"
+    class Time : IComparable<Time>
+    {
+        private int _hours;
+        private int _minutes;
+        private int _seconds;
+
+        public int Hours
+        {
+            get => _hours;
+            set { if (value < 0 || value > 23) throw new ArgumentException(); _hours = value; }
+        }
+
+        public int Minutes
+        {
+            get => _minutes;
+            set { if (value < 0 || value > 59) throw new ArgumentException(); _minutes = value; }
+        }
+
+        public int Seconds
+        {
+            get => _seconds;
+            set { if (value < 0 || value > 59) throw new ArgumentException(); _seconds = value; }
+        }
+
+        public Time(int h, int m, int s)
+        {
+            Hours = h; Minutes = m; Seconds = s;
+        }
+
+        // Метод для получения общего количества секунд (для удобства сравнения)
+        public int ToTotalSeconds() => _hours * 3600 + _minutes * 60 + _seconds;
+
+        // Реализация IComparable: Сортировка по возрастанию времени
+        public int CompareTo(Time other)
+        {
+            if (other == null) return 1;
+            return this.ToTotalSeconds().CompareTo(other.ToTotalSeconds());
+        }
+
+        public override string ToString() => $"{Hours:D2}:{Minutes:D2}:{Seconds:D2}";
+    }
+
+    // Вспомогательный класс для сортировки ТОЛЬКО по секундам (IComparer)
+    class SecondsComparer : IComparer<Time>
+    {
+        public int Compare(Time x, Time y)
+        {
+            return x.Seconds.CompareTo(y.Seconds);
+        }
+    }
+
+    // Вспомогательный класс для сортировки ТОЛЬКО по часам (IComparer)
+    class HoursComparer : IComparer<Time>
+    {
+        public int Compare(Time x, Time y)
+        {
+            return x.Hours.CompareTo(y.Hours);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // Создаем массив объектов времени
+            Time[] times = new Time[]
+            {
+                new Time(15, 30, 10),
+                new Time(10, 10, 50),
+                new Time(23, 00, 05),
+                new Time(05, 45, 30)
+            };
+
+            Console.WriteLine("Исходный список:");
+            PrintArray(times);
+
+            // 1. Сортировка по умолчанию (использует IComparable - хронологически)
+            Array.Sort(times);
+            Console.WriteLine("\nСортировка по умолчанию (хронологическая):");
+            PrintArray(times);
+
+            // 2. Сортировка через IComparer (по секундам)
+            Array.Sort(times, new SecondsComparer());
+            Console.WriteLine("\nСортировка по секундам (используя IComparer):");
+            PrintArray(times);
+
+            // 3. Сортировка через IComparer (по часам)
+            Array.Sort(times, new HoursComparer());
+            Console.WriteLine("\nСортировка по часам (используя IComparer):");
+            PrintArray(times);
+
+            Console.ReadKey();
+        }
+
+        static void PrintArray(Time[] array)
+        {
+            foreach (var t in array) Console.WriteLine(t);
+        }
+    }
+}
